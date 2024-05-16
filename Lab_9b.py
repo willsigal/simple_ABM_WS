a17e056344273feafa9e2831cb130b9f6abf716e
willsigal <164023367+willsigal@users.noreply.github.com>
2024-05-16 13:01:52 -0500

    Update Lab_9b.py

--- Lab_9b.py
+++ Lab_9b.py
@@ -1,3 +1,7 @@
+
+
+
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
@@ -18,39 +22,77 @@ param = {'world_size': (20,20),
          'max_iter'  :5,
          'out_path' : "/Users/willsigal/Documents/GitHub/simple_abm.csv"}
 
+class World:
+    def __init__(self, size):
+        self.size = size
+        self.grid = [[None for i in range(size[1])] for _ in range(size[0])]
+
+    def is_within_bounds(self, loc):
+        return 0 <= loc[0] < self.size[0] and 0 <= loc[1] < self.size[1]
+
+    def is_empty(self, loc):
+        return self.is_within_bounds(loc) and self.grid[loc[0]][loc[1]] is None
+
+    def place_agent(self, agent, loc):
+        self.grid[loc[0]][loc[1]] = agent
+        agent.location = loc
+
+    def remove_agent(self, loc):
+        self.grid[loc[0]][loc[1]] = None
+
+
 class Agent():
-    def __init__(self, world, spot):
+    def __init__(self, world, spot = None):
         self.world = world
         self.spot = spot
         self.location = None
 #Have agent move in random manner, LRUP if Vacant, stay if full        
-def Move(self): 
-    vacancies =self.world.find_vacant(return_all = True)
-    for patch in vacancies:
-        i_moved = False
-        will_i_like_it = self.is_empty(loc=patch)
-        if is_empty is True:
-                    self.world.grid[self.location] = None #move out of current patch
-                    self.location = patch                 #assign new patch to myself
-                    self.world.grid[patch] 
-        i_moved = True
-        
-        return 1
-    else:
-        i_moved = False
-        return 0
-#Make Boolean for whether the spot the agent tries to move to is empty
-def is_empty(self, loc, patch_check = False):
-    if not loc:
-        starting_loc = self.location
-    else:
-        starting_loc = loc
-   neighbor_patches = self.locate_neighbors(starting_loc)
-   neighbor_agents  = [self.world.grid[patch] for patch in neighbor_patches]
-   
+    def move(self):
+        possible_moves = [
+            (self.location[0] - 1, self.location[1]),  # up
+            (self.location[0] + 1, self.location[1]),  # down
+            (self.location[0], self.location[1] - 1),  # left
+            (self.location[0], self.location[1] + 1)   # right
+        ]
+        random.shuffle(possible_moves)
+        for move in possible_moves:
+            if self.world.is_empty(move):
+                self.world.remove_agent(self.location)
+                self.world.place_agent(self, move)
+                break        
             
+def initialize_world(param):
+    world = World(param['world_size'])
+    agents = [Agent(world) for i in range(param['num_agents'])]
+    
+    for agent in agents:
+        while True:
+            loc = (random.randint(0, param['world_size'][0] - 1), 
+                   random.randint(0, param['world_size'][1] - 1))
+            if world.is_empty(loc):
+                world.place_agent(agent, loc)
+                break
+    
+    return world, agents
+
+def simulate(param):
+    world, agents = initialize_world(param)
+    
+    for _ in range(param['max_iter']):
+        for agent in agents:
+            agent.move()
+    
+    return world
+
+def save_world(world, path):
+    data = []
+    for i in range(world.size[0]):
+        for j in range(world.size[1]):
+            if world.grid[i][j] is not None:
+                data.append({'x': i, 'y': j})
+    
+    df = pd.DataFrame(data)
+    df.to_csv(path, index=False)
 
 
-
-
-
+world = simulate(param)
